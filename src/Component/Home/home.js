fbd4dd1377edcdcbac5a42847245c735361a5865
Formsmama <formsmama@Formsmamas-MacBook-Pro.local>
2019-02-13 21:39:08 -0800

    initilal committe

---
+++ src/Component/Home/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Home = ()=> {
+  return(
+    <div> 
+      is this added 
+      Home 
+    </div>
+  )
+};
+
+export default Home;
+
+
---
+++ src/hoc/Layout/layout.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import './layout.css'
+import Header from '../../Component/Header/header';
+
+class Layout extends Component {
+  state= {
+
+
+  }
+  render(){
+    return (
+      <div>
+        <Header/>
+        {this.props.children}
+      </div>
+    )
+  }
+
+}
+export default Layout;
